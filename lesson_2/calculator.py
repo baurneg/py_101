f826e78d2bb4184634ce8398f875c5c0dde558d4
baurneg <baurneg@gmail.com>
2024-07-30 22:49:23 +0000

    calculator updated

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,27 +1,45 @@
-print('Welcome to Calculator!')
-while True:
-    # Ask the user for the first number.
-    print("What's the first number?")
+def prompt(message):
+    print(f"==> {message}")
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
+number1 = input()
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
     number1 = input()
-    
-    # Ask the user for the second number.
-    print("What's the second number?")
+
+prompt("What's the second number?")
+number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
     number2 = input()
-    
-    # Ask the user for an operation to perform.
-    print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
+operation = input()
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt("You must choose 1, 2, 3, or 4")
     operation = input()
-    
-    # Perform the operation on the two numbers.
-    if operation == '1':   # '1' represents addition
+
+match operation:
+    case "1":
         output = int(number1) + int(number2)
-    elif operation == '2': # '2' represents subtraction
+    case "2":
         output = int(number1) - int(number2)
-    elif operation == '3': # '3' represents multiplication
+    case "3":
         output = int(number1) * int(number2)
-    elif operation == '4': # '4' represents division
+    case "4":
         output = int(number1) / int(number2)
 
-    
-    # Print the result to the terminal.
-    print(f"The result is: {output}")
+prompt(f"The result is {output}")
