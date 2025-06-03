ed7a0660c4072f3d9e2e3fbc5bde9976e1ab44fb
Baur Negmetzhanov <baur-mac@Baurs-MacBook-Pro.local>
2025-06-03 15:31:11 -0400

    updated

--- exercises_repeat/easy_1/2.py
+++
@@ -1,9 +0,0 @@
-# Print all odd numbers from 1 to 99, inclusive, with each number on a separate line.
-
-# Bonus Question: Can you solve the problem by iterating over just the odd numbers?
-for number in range(1, 100, 2):
-    print(number)
-
-for number in range(1, 100):
-    if number % 2 != 0:
-        print(number)
